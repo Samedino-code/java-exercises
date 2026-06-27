75ead77e7374b9b27d373819c922062fa68231a7
Sam Börgmann <sam.boergmann@icloud.com>
2026-06-27 15:56:57 +0200

    Add SimpleCalculator class

---
+++ Part-01_Basics/calculations/SimpleCalculator.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Write your program here
+        System.out.println("Give the first number: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Give the second number: ");
+        int secondNumber = scanner.nextInt();
+
+        System.out.println(firstNumber + " + " + secondNumber + " = " + (firstNumber + secondNumber));
+        System.out.println(firstNumber + " - " + secondNumber + " = " + (firstNumber - secondNumber));
+        System.out.println(firstNumber + " * " + secondNumber + " = " + (firstNumber * secondNumber));
+        System.out.println(firstNumber + " / " + secondNumber + " = " + ((double)firstNumber / (double)secondNumber));
+
+    }
+}
